4c9289992b7b90ba610a2fd9c775e1a8487eb337
Bioelectronics System Lab (Shepard Lab) <46267860+klshepard@users.noreply.github.com>
2023-01-29 09:15:54 -0500

    Add files via upload

--- mouse_experiment_estim/mouse_in_vivo_estim_experiment.m
+++ mouse_experiment_estim/mouse_in_vivo_estim_experiment.m
@@ -3,9 +3,13 @@ load('hot_mask.mat')
 
 %% Load Data
 
-% Load first dataset for time series info
-load('IBIS3_Estim_B2.45V_10uA.mat');
+% Load first dataset 
+load('dataset_10uA.mat');
 
+% Average image capture
+figure; imagesc(mean(fr,3).*hot_mask./1022); colorbar('jet'); axis image;
+
+%% Increasing current stim
 % Time series
 tt = 0:1/40:(size(fr,3)-1)/40;
 
@@ -18,7 +22,7 @@ neural_trace = zeros(4,length(tt));
 for ii = [10 25 50 100]
 
     % Load dataset
-    load(['IBIS3_Estim_B2.45V_' num2str(ii,'%0.2d') 'uA.mat']);
+    load(['dataset_' num2str(ii,'%0.2d') 'uA.mat']);
 
     % Remove hot pixel
     fr = fr.*hot_mask;
@@ -68,6 +72,43 @@ set(gca,'FontSize',14);
 
 
 
+%% Spatial Fallout
 
+% Initialize
+nn = 0; ii = [10 25 50 100];
+
+% Synchronize
+tt = [111 72 199 154]+4;
+
+% Plot images
+for aa = 1:length(ii)
+
+    figure;
+
+    % Load data
+    load(['dataset_' num2str(ii(aa),'%0.2d') 'uA.mat']);
+
+    % Remove hot pixel
+    fr = fr.*hot_mask;
+
+    % Remove high variance (light leakage)
+    fr = fr.*(mean(fr,3)>200);
+
+    % Calculate Delta F over F
+    dF_F = (fr-median(fr,3))./median(fr,3);
+
+    % Set Inf to zero
+    dF_F(dF_F == Inf) = 0;
+    dF_F(isnan(dF_F)) = 0;
+
+    % Imagesc plots
+    imagesc(mean(dF_F(:,:,tt(aa):tt(aa)+20),3),[0 0.2]); axis image; 
+
+    % Colormap Jet
+    colormap('jet');
+
+    % Title
+    title(['I_{stim} = ' num2str(ii(aa)) '\muA']);
+end
 
 
